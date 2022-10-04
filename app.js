714c5aa9464c1202771525c5cdc42a0e5aea0884
hyao0007 <hyao0007@student.monash.edu>
2022-10-04 11:07:48 +1100

    Update add task to use DB

--- app.js
+++ app.js
@@ -1,71 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(`${__dirname}/date.js`); // get the module date.js
 const mongoose = require('mongoose');
+const e = require('express');
+const date = require(`${__dirname}/date.js`); // get the module date.js
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// Database config
+const port = 3000;
+
+// --------- DATABASE CONFIG ---------
 mongoose.connect("mongodb://localhost:27017/tasksDB");
-const taskSchema = new mongoose.Schema ({
+const taskSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "A task must have a name"]
     },
-    description: String,
-    dueDate: Date
+    // description: String,
+    // dueDate: Date
 });
 
 // Task model will be added to the tasks collection, and is based off the taskSchema
 const Task = mongoose.model("Task", taskSchema);
 
-const task = new Task({
-    name: "Test task",
-    description: "test task description for mongodb",
-    dueDate: 'today'
+const taskListSchema = new mongoose.Schema({
+    tasks: Array
 });
 
-// task.save(); // saves the task to the 'Tasks' collection in the tasksDB
-// Task.insertMany([list of tasks], (error) => {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         console.log("Successfully added task to tasksDB");
-//     }
-// });
-
-
-
-// Declare variables
-const port = 3000;
-const tasks = [];
-const workTasks = [];
-
+// ------------------------- RUN APPLICATION ------------------------- //
 app.get('/', (req, res) => {
     const day = date.getDate();
-    res.render('list', {listName: day, newTasks: tasks});
+
+    Task.find({}, (error, tasks) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.render('list', {listName: day, newTasks: tasks});
+        }
+    }); // find all tasks
+
+    // console.log(taskNames);
 });
 
 // Runs when a new task is added to the main page through the submit button('/')
 app.post('/', (req, res) => {
-    tasks.push(req.body.newTask);
-    res.redirect('/'); // redirects to the home page
-})
+    const taskName = req.body.newTask;
+    const task = new Task({name: taskName});
+    task.save(); // Add to DB
 
-app.get('/work', (req, res) => {
-    res.render('list', {listName: 'Work To-Do', newTasks: workTasks});
+    res.redirect('/'); // Redirects to the home page to display
 });
 
-app.get('/work', (req, res) => {
-    res.render('list', {listName: 'Work To-Do', newTasks: workTasks});
-});
-
-app.post('/work', (req, res) => {
-    workTasks.push(req.body.newTask);
-    res.redirect('/work');
+app.post('/delete', (req, res) => {
+    console.log(req.body)
 });
 
 app.listen(port, () => {
